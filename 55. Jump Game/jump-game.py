b20e6854d7f54a7a2ce87c0f97e866eaf2575435
Rizal Ilham <rzlilhm09@gmail.com>
2025-03-03 12:02:01 +0700

    55. Jump Game

---
+++ 55. Jump Game/jump-game.py
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/jump-game/?envType=study-plan-v2&envId=top-interview-150
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        gas = 0
+        for n in nums:
+            if gas < 0:
+                return False
+            elif n > gas:
+                gas = n
+            gas -= 1
+         
+            
+        return True
